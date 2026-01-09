85ad2a67d30cc6da19cdcaebb0e55b19c400d1bc
Dawood_Hussain <127896325+dawood90999@users.noreply.github.com>
2026-01-09 16:09:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-circular font-bold text-violet-300'>
+      Home
+    </div>
+  )
+}
+
+export default App
